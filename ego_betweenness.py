190528a5509ad4b886ea6c37d56a7c04c2137c3c
Beau Sievers <beau@beausievers.com>
2017-04-13 22:07:35 -0400

    tie it all together

--- ego_betweenness.py
+++ ego_betweenness.py
@@ -1,5 +1,6 @@
 """Load and analyze an egocentric network."""
 
+import argparse
 import pickle
 import networkx as nx
 
@@ -24,3 +25,18 @@ def ego_betweenness(g, d):
     betweenness = nx.betweenness_centrality(g)
     return betweenness[d['name']]
 
+
+def load(path):
+    """Load data from ego_network.py and print ego betweenness."""
+    d = load_network(path)
+    g = build_graph(d)
+    b = ego_betweenness(g, d)
+    print("Betweenness for {0}: {1}".format(d['name'], b))
+    return b
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args()
+    load(args.path)
