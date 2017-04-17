ac45861b4744bb1b0d9104e05b530154b0086c38
Beau Sievers <beau@beausievers.com>
2017-04-17 17:26:30 -0400

    Changed language; changed how friend entry is finished; added progress indicator for edge entry

--- ego_network.py
+++ ego_network.py
@@ -7,16 +7,17 @@ import pickle
 
 def get_friend(i, n):
     """Ask the user to input the name of a friend."""
-    q = "({0} of {1}) Please enter a name, or enter \"DONE\": ".format(i, n)
-    friend = input(q)
+    q = "({0} of {1}) Please enter a name, or hit enter to finish: ".format(i, n)
+    friend = input(q).strip()
     return friend
 
 
-def get_edge(pair):
+def get_edge(pair, i, n_pairs):
     """Ask the user whether two friends are friends with each other."""
-    q = "Do {0} and {1} like to spend time together? (enter 'y' or 'n') "
-    q = q.format(pair[0], pair[1])
-    response = input(q)
+    q = ("({0} of {1}) Do {2} and {3} often spend time together? "
+         "(enter 'y' or 'n') ")
+    q = q.format(i, n_pairs, pair[0], pair[1])
+    response = input(q).strip()
     valid_responses = ['y', 'ye', 'yes', 'es', '1']
     connected = 0
     if response.lower() in valid_responses:
@@ -43,7 +44,7 @@ def run_survey(max_friends=12):
 
     print("\nPlease carefully read the following.")
 
-    print("\nConsider the people with whom you like to spend your free time. "
+    print("\nConsider the people with whom you often spend your free time. "
           "In the last few months, who are the people you have been with most "
           "often for informal social activities, such as going out to lunch, "
           "dinner, drinks, films, visiting one another's homes, studying "
@@ -53,22 +54,27 @@ def run_survey(max_friends=12):
           "{0} names, enter \"DONE\".\n".format(max_friends))
 
     nodes = list()
-    for i in range(max_friends):
+    i = 0
+    while i < max_friends:
         new_friend = get_friend(i + 1, max_friends)
-        if new_friend.lower() == 'done':
-            break
+        if new_friend == '':
+            q = "Are you done entering names? ('y' or 'n'): "
+            finished = input(q).strip()
+            if finished.lower() == 'y':
+                break
         else:
             nodes.append(new_friend)
+            i += 1
 
     print("\nNow we will ask you to consider each pair of names provided. "
-          "Each of these people is someone who you like to spend time with. "
+          "Each of these people is someone who you often spend time with. "
           "For each pair of people, please indicate whether those two people "
-          "like to spend time with each other. Does this pair of people engage"
+          "often spend time with each other. Does this pair of people engage"
           " in informal social activity, such as going out to lunch, dinner, "
           "drinks, films, visting one another's homes, studying together, "
           "exercising together, and so on?\n")
 
-    pairs = itertools.combinations(nodes, 2)
+    pairs = list(itertools.combinations(nodes, 2))
     edges = []
 
     # Everyone is connected to the ego
@@ -76,8 +82,10 @@ def run_survey(max_friends=12):
         edges.append((name, node))
 
     # Who is connected to whom?
-    for pair in pairs:
-        edge, connected = get_edge(pair)
+    n_pairs = len(pairs)
+
+    for i, pair in enumerate(pairs):
+        edge, connected = get_edge(pair, i + 1, n_pairs)
         if connected == 1:
             edges.append(edge)
 
